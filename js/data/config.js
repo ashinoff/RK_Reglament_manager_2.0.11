949d62a0ca17760816a42af6560f455b6c2195bf
ashinoff <ashinoff@gmail.com>
2025-09-02 13:17:36 +0300

    Create config.js

---
+++ js/data/config.js
@@ -0,0 +1,6 @@
+// Пример добавления нового регламента в config.js:
+regulations: [
+  { id: 'r057-2022', title: 'Р 057-2022...', year: 2022, enabled: true },
+  { id: 'r058-2023', title: 'Р 058-2023...', year: 2023, enabled: true },
+  { id: 'r059-2024', title: 'Р 059-2024...', year: 2024, enabled: false } // пока отключен
+]
